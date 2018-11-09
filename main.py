a3a97b33f09e0215bd4502b7efd76fab8c0bfd93
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-11-09 18:21:58 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import networkx as nx
+from texttable import Texttable
+from parser import parameter_parser
+from spectral_machinery import WaveletMachine
+
+def tab_printer(args):
+    """
+    Function to print the logs in a nice tabular format.
+    :param args: Parameters used for the model.
+    """
+    args = vars(args)
+    keys = sorted(args.keys())
+    tab = Texttable() 
+    tab.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+    print(tab.draw())
+
+def read_graph(path):
+    """
+    Reading the edge list from the path and returning the networkx graph object.
+    :param path: Path to the edge list.
+    :return graph: Graph from edge list.
+    """
+    edge_list = pd.read_csv(path).values.tolist()
+    graph = nx.from_edgelist(edge_list)
+    graph.remove_edges_from(graph.selfloop_edges())
+    return graph
+
+if __name__ == "__main__":
+    
+    settings = parameter_parser()
+    tab_printer(settings)
+    G = read_graph(settings.input)
+    machine = WaveletMachine(G,settings)
+    machine.create_embedding()
+    machine.transform_and_save_embedding()
---
+++ parser.py
@@ -0,0 +1,51 @@
+import argparse
+
+def parameter_parser():
+
+    """
+    A method to parse up command line parameters.
+    """
+
+    parser = argparse.ArgumentParser(description = "Run WaveletMachine.")
+
+    parser.add_argument('--mechanism',
+                        nargs = '?',
+                        default = 'exact',
+	                help = 'Eigenvalue calculation method. Default is exact.')
+
+    parser.add_argument('--input',
+                        nargs = '?',
+                        default = './data/food_edges.csv',
+	                help = 'Path to the graph edges. Default is food_edges.csv.')
+
+    parser.add_argument('--output',
+                        nargs = '?',
+                        default = './output/embedding.csv',
+	                help = 'Path to the structural embedding. Default is embedding.csv.')
+
+    parser.add_argument('--heat-coefficient',
+                        type = float,
+                        default = 1000.0,
+	                help = 'Heat kernel exponent. Default is 1000.0.')
+
+    parser.add_argument('--sample-number',
+                        type = int,
+                        default = 50,
+	                help = 'Number of characteristic function sample points. Default is 50.')
+
+    parser.add_argument('--approximation',
+                        type = int,
+                        default = 100,
+	                help = 'Number of Chebyshev approximation. Default is 100.')
+
+    parser.add_argument('--step-size',
+                        type = int,
+                        default = 20,
+	                help = 'Number of steps. Default is 20.')
+
+    parser.add_argument('--switch',
+                        type = int,
+                        default = 100,
+	                help = 'Number of dimensions. Default is 100.')
+
+    return parser.parse_args()
